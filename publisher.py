a126ffe769a7b8b425a6a6f82e40d0d674b24afc
gordon0518 <gord0518@gmail.com>
2025-07-28 10:47:31 +0800

    Initial commit

---
+++ publisher.py
@@ -0,0 +1,63 @@
+import paho.mqtt.client as mqtt
+
+class Publisher:
+
+    def __init__(self, broker):
+        self.broker = broker
+
+
+    ##Sending Text
+    def send_str(self, topic, message):
+        client = mqtt.Client()
+        try:
+            client.connect(self.broker, 1883, 60)
+            client.loop_start()
+            result = client.publish(topic, message)
+            if result.rc == mqtt.MQTT_ERR_SUCCESS:
+                print("Successfully published")
+            else:
+                print("Failed to publish")
+        except Exception as e:
+            print(e)
+        finally:
+            client.loop_stop()
+            client.disconnect()
+
+    ##Sending txt File
+    def send_txt(self, topic, path):
+        client = mqtt.Client()
+
+        try:
+            client.connect(self.broker, 1883, 60)
+            client.loop_start()
+
+            with open(path, "r") as f:
+                txt = f.read()
+                result = client.publish(topic, txt)
+            if result.rc == mqtt.MQTT_ERR_SUCCESS:
+                print("Successfully published")
+            else:
+                print("Failed to publish")
+        except Exception as e:
+            print(e)
+
+        finally:
+            client.loop_stop()
+            client.disconnect()
+
+if __name__ == '__main__':
+    device = Publisher("broker.hivemq.com")
+
+    txt_topic = "testmee/txt"
+    string_topic = "testmee/string"
+
+    msg = "testing"
+    path = "test.txt"
+
+    device.send_str(string_topic, msg)
+
+    device.send_txt(txt_topic, path)
+
+
+
+
---
+++ subcriber.py
@@ -0,0 +1,74 @@
+import paho.mqtt.client as mqtt
+import asyncio
+
+
+class Subscriber:
+
+    def __init__(self, broker):
+        self.broker = broker
+
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to broker")
+        else:
+            print(f"Failed to connect: {rc}")
+
+    def on_message(self, client, userdata, message):
+        print(message.payload.decode("utf-8"))
+
+    def on_message_txt(self, client, userdata, message):
+        with open("Recieved.txt", "w") as f:
+            f.write(message.payload.decode("utf-8"))
+        print("File Saved")
+
+
+    ##Recieving String
+    async def get_string(self, topic):
+        client = mqtt.Client()
+        try:
+            client.on_connect = self.on_connect
+            client.on_message = self.on_message
+            client.connect(self.broker, 1883, 60)
+            client.loop_start()
+            client.subscribe(topic)
+
+            await asyncio.sleep(60)
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            client.loop_stop()
+            client.disconnect()
+
+    ##Recieving txt file
+    async def get_txt(self, topic):
+        client = mqtt.Client()
+        try:
+            client.on_connect = self.on_connect
+            client.on_message = self.on_message_txt
+            client.connect(self.broker, 1883, 60)
+            client.loop_start()
+            client.subscribe(topic)
+
+            await asyncio.sleep(60)
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            client.loop_stop()
+            client.disconnect()
+
+
+async def main():
+    broker = "broker.hivemq.com"
+    string_topic = "testmee/string"
+    txt_topic = "testmee/txt"
+    device = Subscriber(broker)
+
+    await asyncio.gather(device.get_txt(txt_topic), device.get_string(string_topic))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
