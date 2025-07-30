ca77ad606c76872d21577ff6f74baa38e7904d30
gordon0518 <gord0518@gmail.com>
2025-07-30 16:38:39 +0800

    response to publisher and allow transfer exe file

--- publisher.py
+++ publisher.py
@@ -1,63 +1,119 @@
 import paho.mqtt.client as mqtt
+import asyncio
 
 class Publisher:
 
     def __init__(self, broker):
         self.broker = broker
 
+    ## Sending txt File
+    async def send_txt(self, topic, path):
+        client = mqtt.Client()
+        try:
+            client.connect(self.broker, 1883, 60)
+            client.loop_start()
+            with open(path, "r") as f:
+                txt = f.read()
+                result = client.publish(topic, txt)
+            if result.rc == mqtt.MQTT_ERR_SUCCESS:
+                print("Successfully published txt")
+                await self.str_response(topic)
+            else:
+                print("Failed to publish txt")
+        except Exception as e:
+            print(e)
+        finally:
+            client.loop_stop()
+            client.disconnect()
 
-    ##Sending Text
-    def send_str(self, topic, message):
+    ## Sending file
+    async def send_file(self, topic, path):
+        client = mqtt.Client()
+        try:
+            client.connect(self.broker, 1883, 60)
+            client.loop_start()
+            with open(path, "rb") as f:
+                file = f.read()
+                result = client.publish(topic, file)
+            if result.rc == mqtt.MQTT_ERR_SUCCESS:
+                print("Successfully published file")
+                await self.str_response(topic)
+            else:
+                print("Failed to publish file")
+        except Exception as e:
+            print(e)
+        finally:
+            client.loop_stop()
+            client.disconnect()
+
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to broker")
+        else:
+            print(f"Failed to connect: {rc}")
+
+    def on_message(self, client, userdata, message):
+        print("Received Message: " + message.payload.decode("utf-8"))
+
+    ##Handing getting response
+    async def str_response(self, topic):
+        client = mqtt.Client()
+        try:
+            client.on_connect = self.on_connect
+            client.on_message = self.on_message
+            client.connect(self.broker, 1883, 60)
+            client.loop_start()
+            client.subscribe(topic + "/response")
+            await asyncio.sleep(10)
+        except Exception as e:
+            print(e)
+        finally:
+            client.loop_stop()
+            client.disconnect()
+
+    ## Sending Text
+    async def send_str(self, topic, message):
         client = mqtt.Client()
         try:
             client.connect(self.broker, 1883, 60)
             client.loop_start()
             result = client.publish(topic, message)
             if result.rc == mqtt.MQTT_ERR_SUCCESS:
-                print("Successfully published")
+                print("Successfully published string")
+                await self.str_response(topic)
             else:
-                print("Failed to publish")
+                print("Failed to publish string")
         except Exception as e:
             print(e)
         finally:
             client.loop_stop()
             client.disconnect()
 
-    ##Sending txt File
-    def send_txt(self, topic, path):
-        client = mqtt.Client()
 
-        try:
-            client.connect(self.broker, 1883, 60)
-            client.loop_start()
-
-            with open(path, "r") as f:
-                txt = f.read()
-                result = client.publish(topic, txt)
-            if result.rc == mqtt.MQTT_ERR_SUCCESS:
-                print("Successfully published")
-            else:
-                print("Failed to publish")
-        except Exception as e:
-            print(e)
-
-        finally:
-            client.loop_stop()
-            client.disconnect()
-
-if __name__ == '__main__':
+##Testing
+async def main():
+    ##broker
     device = Publisher("broker.emqx.io")
 
+    ##Topic
     txt_topic = "testmee/txt"
     string_topic = "testmee/string"
+    file_topic = "testmee/file"
 
     msg = "testing"
     path = "test.txt"
+    exe_path = "record.py"
 
-    device.send_str(string_topic, msg)
+    await asyncio.gather(
+        device.send_str(string_topic, msg),
+        device.send_txt(txt_topic, path),
+        # device.send_file(file_topic, exe_path)
+    )
 
-    device.send_txt(txt_topic, path)
-
-
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Publisher stopped")
 
 
--- subscriber.py
+++ subscriber.py
@@ -1,11 +1,13 @@
 import paho.mqtt.client as mqtt
 import asyncio
-
+import publisher
 
 class Subscriber:
 
-    def __init__(self, broker):
+    def __init__(self, broker, loop):
         self.broker = broker
+        self.publisher = publisher.Publisher(broker)
+        self.loop = loop
 
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
@@ -13,66 +15,115 @@ class Subscriber:
         else:
             print(f"Failed to connect: {rc}")
 
+    ##handing string received
     def on_message(self, client, userdata, message):
-        print("Recieved Message: " + message.payload.decode("utf-8"))
+        topic = message.topic
+        payload = message.payload.decode("utf-8")
+        print("Received Message: " + payload)
+
+        asyncio.run_coroutine_threadsafe(
+            self.publisher.send_str(topic + "/response", "received"), self.loop
+        )
+
+    ##Handing txt file received
+    async def on_message_txt_async(self, topic, payload):
+        try:
+            with open("Received.txt", "w") as f:
+                f.write(payload)
+            print("File Saved")
+            await asyncio.sleep(3)
+            await self.publisher.send_str(topic + "/response", "received")
+        except Exception as e:
+            print(f"Error saving text file: {e}")
 
     def on_message_txt(self, client, userdata, message):
-        with open("Recieved.txt", "w") as f:
-            f.write(message.payload.decode("utf-8"))
-        print("File Saved")
+        topic = message.topic
+        payload = message.payload.decode("utf-8")
+
+        asyncio.run_coroutine_threadsafe(
+            self.on_message_txt_async(topic, payload), self.loop
+        )
 
 
-    ##Recieving String
+    ##handing exe file received
+    def on_message_file(self, client, userdata, message):
+        topic = message.topic
+        try:
+            with open("Received.exe", "ab") as f:
+                f.write(message.payload)
+            print("File Saved")
+
+            asyncio.run_coroutine_threadsafe(
+                self.publisher.send_str(topic + "/response", "received"), self.loop
+            )
+        except Exception as e:
+            print(f"Error saving file: {e}")
+
+    ## Receiving String
     async def get_string(self, topic):
         client = mqtt.Client()
         try:
-
             client.on_connect = self.on_connect
             client.on_message = self.on_message
             client.connect(self.broker, 1883, 60)
-            while(True):
-                client.loop_start()
-                client.subscribe(topic)
+            client.subscribe(topic)
+            client.loop_start()
 
-                await asyncio.sleep(10)
-
+            await asyncio.Event().wait()
         except Exception as e:
             print(e)
-
         finally:
             client.loop_stop()
             client.disconnect()
 
-    ##Recieving txt file
+    ## Receiving txt file
     async def get_txt(self, topic):
         client = mqtt.Client()
         try:
-            while(True):
-                client.on_connect = self.on_connect
-                client.on_message = self.on_message_txt
-                client.connect(self.broker, 1883, 60)
-                while(True):
-                    client.loop_start()
-                    client.subscribe(topic)
+            client.on_connect = self.on_connect
+            client.on_message = self.on_message_txt
+            client.connect(self.broker, 1883, 60)
+            client.subscribe(topic)
+            client.loop_start()
 
-                    await asyncio.sleep(10)
-
+            await asyncio.Event().wait()
         except Exception as e:
             print(e)
+        finally:
+            client.loop_stop()
+            client.disconnect()
 
+    ## Receiving file
+    async def get_file(self, topic):
+        client = mqtt.Client()
+        try:
+            client.on_connect = self.on_connect
+            client.on_message = self.on_message_file
+            client.connect(self.broker, 1883, 60)
+            client.subscribe(topic)
+            client.loop_start()
+            await asyncio.Event().wait()
+        except Exception as e:
+            print(e)
         finally:
             client.loop_stop()
             client.disconnect()
 
 
+##Testing
 async def main():
+    loop = asyncio.get_running_loop()
     broker = "broker.emqx.io"
     string_topic = "testmee/string"
     txt_topic = "testmee/txt"
-    device = Subscriber(broker)
+    file_topic = "testmee/file"
 
+    device = Subscriber(broker, loop)
     await asyncio.gather(device.get_txt(txt_topic), device.get_string(string_topic))
 
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Subscriber stopped")
+
